68d632dc506025a0156915b9548aba64d450182f
Amar Dasari <amardasari84@gmail.com>
2024-06-25 23:15:56 +0530

    rock_paper_scissor-game

---
+++ absolute differsnce.py
@@ -0,0 +1,8 @@
+#absolute difference is the difference wiithout negative sign
+num1=int(input())
+num2=int(input())
+if num1>num2:
+    print(num1-num2)
+else:
+    diff=num1-num2
+    print(-(diff))
---
+++ assigment2bnxtwave.py
@@ -0,0 +1,3 @@
+num=input()
+same=int(num[0])==int(num[1])==int(num[2])
+print(same)
